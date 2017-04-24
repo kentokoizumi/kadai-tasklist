1d0815d5c3aa4c7cac1cc288a83b674578fc65ad
Kento Koizumi <kairiseisyougai@gmail.com>
2017-04-24 01:22:04 +0000

    post

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  root to: 'tasks#index'
+  root to: 'toppages#index'
   
-  resources :tasks
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  
+  get 'signup', to: 'users#new'
+  resources :tasks, only: [:update, :show, :create, :edit, :destroy]
+  resources :users, only: [:show, :new, :create]
 end
